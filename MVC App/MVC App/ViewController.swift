2da8d96bd6234d67727191387120f9ca27e9003c
VasckMe <kasarin.anton@gmail.com>
2022-08-24 17:40:31 +0300

    Project created

---
+++ MVC App/MVC App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC App
+//
+//  Created by Apple Macbook Pro 13 on 24.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
